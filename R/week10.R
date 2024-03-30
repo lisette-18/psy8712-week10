c950ceb639d59875ca87bc0a1ef7010f3843d9b4
lisette-18 <horne146@umn.edu>
2024-03-30 17:38:53 -0500

    Initial Commit

---
+++ R/week10.R
@@ -0,0 +1,14 @@
+#Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(haven)
+library(tidyverse)
+library(caret)
+
+#Data Import and Cleaning
+
+#Visualization
+
+#Analysis 
+
+
+#Publication
