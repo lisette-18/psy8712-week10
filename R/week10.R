a7195f30f5ca1ff1a8a4428c2fd60067188e7436
lisette-18 <horne146@umn.edu>
2024-03-30 18:42:12 -0500

    Visualization

--- R/week10.R
+++ R/week10.R
@@ -10,12 +10,13 @@ gss_tbl <- read_sav("../data/GSS2016.sav") %>%
   filter(!is.na(MOSTHRS)) %>%
   rename("work hours" = MOSTHRS) %>%
   select(-HRS1, -HRS2) %>%
-  select(-where(~ mean(is.na(.)) > 0.75)) %>%
+  select(-where(~mean(is.na(.))>.75)) %>%
   mutate_all(as.numeric)
   
-  
-
 #Visualization
+ggplot(gss_tbl, aes(x = `work hours`)) +
+  geom_histogram(binwidth = 5, fill = "pink") +
+  labs(title = "Distribution of Work Hours", x = "Work Hours", y = "Frequency")
 
 #Analysis 
 
