604b20aed2858d0df1f38482bead57a8d0ffe5e5
lisette-18 <horne146@umn.edu>
2024-03-30 18:27:30 -0500

    Data Import and Cleaning

--- R/week10.R
+++ R/week10.R
@@ -5,6 +5,15 @@ library(tidyverse)
 library(caret)
 
 #Data Import and Cleaning
+gss_tbl <- read_sav("../data/GSS2016.sav") %>%
+  mutate_all(~ifelse(.==0, NA, .)) %>% 
+  filter(!is.na(MOSTHRS)) %>%
+  rename("work hours" = MOSTHRS) %>%
+  select(-HRS1, -HRS2) %>%
+  select(-where(~ mean(is.na(.)) > 0.75)) %>%
+  mutate_all(as.numeric)
+  
+  
 
 #Visualization
 
